b207d62fac1d87937ebdf601aa53dfdaa6cc93c1
Sergey Kazulin <SergeyKazulin@hotmail.com>
2020-06-29 16:04:54 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace DivisionBy3and7Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int number;
+            Console.WriteLine("Введите целое число: ");
+            number = Int32.Parse(Console.ReadLine());
+            if (number % 3 == 0 & number % 5 == 0)
+                Console.WriteLine("FizzBuzz");
+            
+            else if (number % 3 == 0)
+                Console.WriteLine("Fizz");
+            
+            else if (number % 5 == 0)
+                Console.WriteLine("Buzz");
+            
+            else 
+                Console.WriteLine("No Fizz, No Buzz");
+            
+            Console.ReadKey();
+
+        }    
+           
+    }
+
+}
